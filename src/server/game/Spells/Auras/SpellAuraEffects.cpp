7643e57c82e7d890c5ff6b60ed7bc882856f641b
Bootz <Stage6Dev@EMPulseGaming.com>
2011-08-26 18:26:15 +0800

    Core/Spells: Fixed the aura for AuraHover
    Core/Gameobjects: Fixed ASSERT closed when deleting GO

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -4244,9 +4244,9 @@ void AuraEffect::HandleAuraHover(AuraApplication const *aurApp, uint8 mode, bool
 
     WorldPacket data;
     if (apply)
-        data.Initialize(SMSG_MOVE_SET_HOVER, 8+4);
+        data.Initialize(SMSG_MOVE_SET_HOVER, 8+4, true);
     else
-        data.Initialize(SMSG_MOVE_UNSET_HOVER, 8+4);
+        data.Initialize(SMSG_MOVE_UNSET_HOVER, 8+4, true);
     data.append(target->GetPackGUID());
     data << uint32(0);
     target->SendMessageToSet(&data, true);
