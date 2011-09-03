37bddec0af1f53d91bea76566b0e8bdec8caea49
Victor MindBreaker <victor611@yahoo.com>
2011-09-04 05:56:12 +0800

    DB/Templates: Fixed Argaloth health

---
+++ sql/updates/world/11_09_03_world_creature_template.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `Health_mod`=348.12989413271509865274557805483 WHERE `entry`=47120; -- Argaloth 10
+UPDATE `creature_template` SET `Health_mod`=1044.3896823981452959582367341645 WHERE `entry`=51350; -- Argaloth 25
